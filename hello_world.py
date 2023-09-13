dbfdb215f91102e3d61dc9ef20b0d436708c380a
kimdonghkyuek <20230138@kumoh.ac.kr>
2023-09-13 15:38:22 +0900

    git_test3.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,11 @@
-def print_hello():
-    print("Hello World!!")
+def add(x, y):
+    a = x+y
+    return a
 
 def print_KIT():
-    print("kumoh National Insitute of Technology.")
-
+    print("KIT.")
 
 if __name__ == '__main__':
     print_KIT()
-    print_hello()
+    print_hello(1, 2)
     
